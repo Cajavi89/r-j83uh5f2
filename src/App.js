bc63e0de870f66bb7dea741cd6c335b479bf93f9
Carlos <cajavi89@gmail.com>
2021-11-29 19:53:20 -0500

    reto react Contador de caracteres textArea

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      counter:0
+    }
+    this.contadorInputs = this.contadorInputs.bind(this)
+  }
+
+  contadorInputs(e){
+
+    this.setState({
+      counter:e.target.value.length
+      })
+    // console.log(e.target.value.length)
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea className="textArea" onChange={this.contadorInputs} rows="3"></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
   }
